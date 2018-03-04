dbebc958938132963c397f15556f1a40479859fb
Francesco Giordano <fgiordano@mathspace.com.au>
2018-03-04 20:18:44 +1100

    Fix

--- src/modules/Canvas/Circle.jsx
+++ src/modules/Canvas/Circle.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const { sqrt, pow } = Math;
 const distance = (p1, p0) => sqrt(pow(p1.x - p0.x, 2) + pow(p1.y - p0.y, 2));
 
-const Circle = ({ x0, y0, x1 = x0, y1 = y0, color, thickness }) => (
+const Circle = ({ x0, y0, x1, y1, color, thickness }) => (
   <circle
     cx={x0}
     cy={y0}
