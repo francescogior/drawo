e827fe7fbdaddf09dda8dc344b0aab2348c2c7cf
Francesco Giordano <fgiordano@mathspace.com.au>
2018-08-25 15:51:01 +1000

    Cleanup

--- src/modules/Canvas/Circle.jsx
+++ src/modules/Canvas/Circle.jsx
@@ -1,17 +1,20 @@
-import React from "react";
+import React from 'react'
 
-const { sqrt, pow } = Math;
-const distance = (p1, p0) => sqrt(pow(p1.x - p0.x, 2) + pow(p1.y - p0.y, 2));
+const { sqrt, pow } = Math // eslint-disable-line
+const distance = (p1, p0) => sqrt(pow(p1.x - p0.x, 2) + pow(p1.y - p0.y, 2))
 
-const Circle = ({ x0, y0, x1, y1, r, color, thickness }) => (
+const Circle = ({
+  x0, y0, x1, y1, r, color, thickness,
+}) => (
   <circle
     cx={x0}
     cy={y0}
-    r={typeof r === "number" ? r : distance({ x: x1, y: y1 }, { x: x0, y: y0 })}
-    fill={"transparent"}
+    r={typeof r === 'number' ? r : distance({ x: x1, y: y1 }, { x: x0, y: y0 })}
+    fill="transparent"
     strokeWidth={thickness}
     stroke={color}
+    style={{ cursor: 'pointer' }}
   />
-);
+)
 
-export default Circle;
+export default Circle
